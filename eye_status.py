2d4913b2bab30d27251156bf4779e550dde97e6f
Tien <viettienha98@gmail.com>
2019-08-19 16:24:40 +0700

    Modify

--- eye_status.py
+++ eye_status.py
@@ -19,7 +19,7 @@ def collect():
 	train_datagen = ImageDataGenerator(
 			rescale=1./255,
 			shear_range=0.2,
-			horizontal_flip=True, 
+			horizontal_flip=True,
 		)
 
 	val_datagen = ImageDataGenerator(
@@ -48,30 +48,12 @@ def collect():
 	)
 	return train_generator, val_generator
 
-
-def save_model(model):
-	model_json = model.to_json()
-	with open("model.json", "w") as json_file:
-		json_file.write(model_json)
-	# serialize weights to HDF5
-	model.save_weights("model.h5")
-
-def load_model():
-	json_file = open('model.json', 'r')
-	loaded_model_json = json_file.read()
-	json_file.close()
-	loaded_model = model_from_json(loaded_model_json)
-	# load weights into new model
-	loaded_model.load_weights("model.h5")
-	loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return loaded_model
-
 def train(train_generator, val_generator):
 	STEP_SIZE_TRAIN=train_generator.n//train_generator.batch_size
 	STEP_SIZE_VALID=val_generator.n//val_generator.batch_size
 
 	print('[LOG] Intialize Neural Network')
-	
+
 	model = Sequential()
 
 	model.add(Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=(IMG_SIZE,IMG_SIZE,1)))
@@ -90,14 +72,14 @@ def train(train_generator, val_generator):
 
 
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
+	checkpointer = ModelCheckpoint(filepath='models/eye_model.{epoch:02d}-{val_loss:.2f}.hdf5',
+                               verbose=1, save_best_only=True)
 	model.fit_generator(generator=train_generator,
 	                    steps_per_epoch=STEP_SIZE_TRAIN,
 	                    validation_data=val_generator,
 	                    validation_steps=STEP_SIZE_VALID,
 	                    epochs=20
 	)
-	save_model(model)
 
 def predict(img, model):
 	img = Image.fromarray(img, 'RGB').convert('L')
@@ -119,6 +101,6 @@ def evaluate(X_test, y_test):
 	loss, acc = model.evaluate(X_test, y_test, verbose = 0)
 	print(acc * 100)
 
-if __name__ == '__main__':	
+if __name__ == '__main__':
 	train_generator , val_generator = collect()
 	train(train_generator,val_generator)
